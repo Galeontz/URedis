4c7404394259eb6a13512c9fa2d28f2927583756
Paolo Galeone <nessuno@nerdz.eu>
2024-05-11 13:29:14 +0200

    Repository structure and linux support (#1)
    * rider ignore
    
    * git lfs settings
    
    * basic structure with linux support
    
    * track .so (with git lfs) and follow symlinks

---
+++ Source/ThirdParty/URedisLibrary/URedisLibrary.Build.cs
@@ -0,0 +1,22 @@
+using System.IO;
+using UnrealBuildTool;
+
+public class URedisLibrary : ModuleRules {
+  public URedisLibrary(ReadOnlyTargetRules Target) : base(Target) {
+    Type = ModuleType.External;
+
+    PublicIncludePaths.Add(Path.Combine(ModuleDirectory, "include"));
+
+    if (Target.Platform == UnrealTargetPlatform.Linux) {
+      var libname = "libredis++.so";
+      var path = Path.Combine(ModuleDirectory, "linux", "x64", libname);
+      PublicAdditionalLibraries.Add(path);
+      RuntimeDependencies.Add(path);
+      
+      libname = "libhiredis.so";
+      path = Path.Combine(ModuleDirectory, "linux", "x64", libname);
+      PublicAdditionalLibraries.Add(path);
+      RuntimeDependencies.Add(path);
+    }
+  }
+}
---
+++ Source/URedis/Private/URedis.cpp
@@ -0,0 +1,41 @@
+#include "URedis.h"
+
+#include "HAL/Platform.h"
+
+#if PLATFORM_WINDOWS
+#include "Windows/AllowWindowsPlatformTypes.h"
+#include "Windows/WindowsHWrapper.h"
+#endif
+
+THIRD_PARTY_INCLUDES_START
+#include "sw/redis++/redis.h"
+THIRD_PARTY_INCLUDES_END
+
+#if PLATFORM_WINDOWS
+#include "Windows/HideWindowsPlatformTypes.h"
+#endif
+
+#include "Modules/ModuleManager.h"
+
+#include <string>
+
+void FURedis::Connect(const FStringView host, const int32 port) {
+  sw::redis::ConnectionOptions opts;
+  opts.host = TCHAR_TO_UTF8(host.GetData());
+  opts.port = port;
+  _instance = MakeUnique<sw::redis::Redis>(opts);
+  check(_instance);
+}
+
+FString FURedis::Ping(TOptional<FString> message) const {
+  check(_instance);
+  std::string reply{};
+  if (message) {
+    reply = _instance->echo(TCHAR_TO_UTF8(**message));
+  } else {
+    reply = _instance->ping();
+  }
+  return UTF8_TO_TCHAR(reply.c_str());
+}
+
+IMPLEMENT_MODULE(FURedis, URedis);
