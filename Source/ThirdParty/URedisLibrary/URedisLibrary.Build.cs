d0f7baecc8316bf40d2c78dc228798dbe75a3578
EliLentz <feedapps.royal@gmail.com>
2024-05-16 17:22:22 +0300

    Added windows support + changed file structure
    * Added windows libraries
    * Replaced linux libraries
    * Added windows support to .build file

--- Source/ThirdParty/URedisLibrary/URedisLibrary.Build.cs
+++ Source/ThirdParty/URedisLibrary/URedisLibrary.Build.cs
@@ -1,13 +1,16 @@
 using System.IO;
 using UnrealBuildTool;
 
-public class URedisLibrary : ModuleRules {
-  public URedisLibrary(ReadOnlyTargetRules Target) : base(Target) {
+public class URedisLibrary : ModuleRules
+{
+  public URedisLibrary(ReadOnlyTargetRules Target) : base(Target)
+  {
     Type = ModuleType.External;
 
-    PublicIncludePaths.Add(Path.Combine(ModuleDirectory, "include"));
+    if (Target.Platform == UnrealTargetPlatform.Linux)
+    {
+      PublicIncludePaths.Add(Path.Combine(ModuleDirectory, "include", "linux"));
 
-    if (Target.Platform == UnrealTargetPlatform.Linux) {
       var libname = "libredis++.so";
       var path = Path.Combine(ModuleDirectory, "linux", "x64", libname);
       PublicAdditionalLibraries.Add(path);
@@ -17,6 +20,22 @@ public class URedisLibrary : ModuleRules {
       path = Path.Combine(ModuleDirectory, "linux", "x64", libname);
       PublicAdditionalLibraries.Add(path);
       RuntimeDependencies.Add(path);
+    }
+    else if (Target.Platform == UnrealTargetPlatform.Win64)
+    {
+      PublicIncludePaths.Add(Path.Combine(ModuleDirectory, "include", "windows"));
+
+      var libName = "hiredis.lib";
+      var dllName = "hiredis.dll";
+
+      PublicAdditionalLibraries.Add(Path.Combine(ModuleDirectory, "windows", "x64", libName));
+      RuntimeDependencies.Add(Path.Combine(PluginDirectory, "Binaries", "Win64", dllName));
+
+      libName = "redis++.lib";
+      dllName = "redis++.dll";
+
+      PublicAdditionalLibraries.Add(Path.Combine(ModuleDirectory, "windows", "x64", libName));
+      RuntimeDependencies.Add(Path.Combine(PluginDirectory, "Binaries", "Win64", dllName));
     }
   }
 }
--- Source/URedis/URedis.Build.cs
+++ Source/URedis/URedis.Build.cs
@@ -1,14 +1,17 @@
 using UnrealBuildTool;
 
-public class URedis : ModuleRules {
-  public URedis(ReadOnlyTargetRules Target) : base(Target) {
-    PCHUsage = PCHUsageMode.UseExplicitOrSharedPCHs;
+public class URedis : ModuleRules
+{
+    public URedis(ReadOnlyTargetRules Target) : base(Target)
+    {
+        PCHUsage = PCHUsageMode.UseExplicitOrSharedPCHs;
 
-    DefaultBuildSettings = BuildSettingsVersion.V5;
+        DefaultBuildSettings = BuildSettingsVersion.V5;
 
-    PublicDependencyModuleNames.AddRange(new[] {
-      "Core",
-      "URedisLibrary",
-    });
-  }
+        PublicDependencyModuleNames.AddRange(new[] {
+            "Core",
+            "URedisLibrary"
+            }
+        );
+    }
 }
