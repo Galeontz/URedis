17ef4d3352dcfca5bb9d13833d659a6d29686702
EliLentz <feedapps.royal@gmail.com>
2024-05-16 23:27:16 +0300

    Added C++ 17 standart version

--- Source/URedis/URedis.Build.cs
+++ Source/URedis/URedis.Build.cs
@@ -3,6 +3,7 @@ using UnrealBuildTool;
 public class URedis : ModuleRules {
   public URedis(ReadOnlyTargetRules Target) : base(Target) {
     PCHUsage = PCHUsageMode.UseExplicitOrSharedPCHs;
+        CppStandard = CppStandardVersion.Cpp17;
 
     DefaultBuildSettings = BuildSettingsVersion.V5;
 
