ed9216c63a38c51c92a592fdc5b9cf862fbc8aa3
EliLentz <feedapps.royal@gmail.com>
2024-05-16 23:27:58 +0300

    small changes

--- Source/URedis/URedis.Build.cs
+++ Source/URedis/URedis.Build.cs
@@ -1,12 +1,14 @@
 using UnrealBuildTool;
 
-public class URedis : ModuleRules {
-  public URedis(ReadOnlyTargetRules Target) : base(Target) {
-    PCHUsage = PCHUsageMode.UseExplicitOrSharedPCHs;
+public class URedis : ModuleRules
+{
+    public URedis(ReadOnlyTargetRules Target) : base(Target)
+    {
+        PCHUsage = PCHUsageMode.UseExplicitOrSharedPCHs;
         CppStandard = CppStandardVersion.Cpp17;
 
-    DefaultBuildSettings = BuildSettingsVersion.V5;
+        DefaultBuildSettings = BuildSettingsVersion.V5;
 
-    PublicDependencyModuleNames.AddRange(new[] { "Core", "URedisLibrary" });
-  }
+        PublicDependencyModuleNames.AddRange(new[] { "Core", "URedisLibrary" });
+    }
 }
