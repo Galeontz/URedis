92730aac90df3bac0c52f588ccb05886fc4d7d81
Paolo Galeone <nessuno@nerdz.eu>
2024-05-16 18:49:32 +0200

    fix formatting

--- Source/URedis/URedis.Build.cs
+++ Source/URedis/URedis.Build.cs
@@ -1,17 +1,11 @@
 using UnrealBuildTool;
 
-public class URedis : ModuleRules
-{
-    public URedis(ReadOnlyTargetRules Target) : base(Target)
-    {
-        PCHUsage = PCHUsageMode.UseExplicitOrSharedPCHs;
+public class URedis : ModuleRules {
+  public URedis(ReadOnlyTargetRules Target) : base(Target) {
+    PCHUsage = PCHUsageMode.UseExplicitOrSharedPCHs;
 
-        DefaultBuildSettings = BuildSettingsVersion.V5;
+    DefaultBuildSettings = BuildSettingsVersion.V5;
 
-        PublicDependencyModuleNames.AddRange(new[] {
-            "Core",
-            "URedisLibrary"
-            }
-        );
-    }
+    PublicDependencyModuleNames.AddRange(new[] { "Core", "URedisLibrary" });
+  }
 }
