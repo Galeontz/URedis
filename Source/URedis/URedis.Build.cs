03937be39aa9687ace3ac14f9c9ffdc9871cdd40
Paolo Galeone <nessuno@nerdz.eu>
2024-05-18 10:47:40 +0200

    Revert "Added C++ 17 standart version"
    This reverts commit 17ef4d3352dcfca5bb9d13833d659a6d29686702.

--- Source/URedis/URedis.Build.cs
+++ Source/URedis/URedis.Build.cs
@@ -3,7 +3,6 @@ using UnrealBuildTool;
 public class URedis : ModuleRules {
   public URedis(ReadOnlyTargetRules Target) : base(Target) {
     PCHUsage = PCHUsageMode.UseExplicitOrSharedPCHs;
-        CppStandard = CppStandardVersion.Cpp17;
 
     DefaultBuildSettings = BuildSettingsVersion.V5;
 
